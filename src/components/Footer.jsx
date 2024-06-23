01a75c6c3906fa705dbf663521abd366a612ce3d
ajkun55 <81357643+ajkun55@users.noreply.github.com>
2024-06-23 16:33:54 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">John A</a>.
+        Coded by <a href="https://github.com/ajkun55/Room-homepage">John A</a>.
   </div>
   )
 }
